ca533be1f9ebb96d430f88c89a5ad30e22e63b78
SebanchoA <jsag98@gmail.com>
2022-10-10 15:56:06 -0700

    Pos Neg

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -5,20 +5,20 @@
 
 int main(void)
 {
-  int n, lastDigit;
+  int n, l;
   
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	lastDigit = n % 10;
-  if ( n > 5)
-    printf ( "Last digit of\n", n, "is\n", lastDigit, "and is greater that 5\n");
+	l = n % 10;
+  if ( l > 5)
+    printf ( "Last digit of\n %i is %i and is greater that 5\n", n, l);
       
   else
-    if (n < 6 && n != 0)
-      printf ("Last digit of\n", n, "is\n", lastDigit, "and is less that 6 and not 0");
+    if (l < 6)
+      printf ("Last digit of %i is %i and is less that 6 and not 0}n", n , l);
     else
-      if (n==0)
-	printf ("Last digit of\n", n, "is\n", lastDigit, "and is 0\n");
+      if (l==0)
+	printf ("Last digit of %i is %i and is 0\n", n, l);
   return (0);
 }
