c23f6021b7f236bb4df8208c43d813718ee31123
SebanchoA <jsag98@gmail.com>
2022-10-10 16:31:08 -0700

    Alphabts

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,9 @@ int main(void)
 {
   char ch;
 
-  for (ch = 'a' && ch != 'q''e'; ch <= 'z' ; ch++)
+  for (ch = 'a'; ch <= 'z' ; ch++)
     {
-      
+      if (ch != 'e' && ch != 'q')
     putchar (ch);
     }
   putchar ('\n');
