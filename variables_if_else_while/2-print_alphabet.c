32e07205adf11363be34574050b4b44d0bc1ac31
SebanchoA <jsag98@gmail.com>
2022-10-10 16:11:12 -0700

    Alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-
+/**
+ *main - alphabet
+ *
+ *Return: 0
+ */
 int main(void)
 {
-  char ch='1';
+  char ch;
 
   for (ch = 'a'; ch <= 'z' : ch++)
+    {
     putchar (ch);
-
+    }
+  putchar ('\n');
   return (0);
 }
