77202ed3788a03e337261338f179859fc51284f6
SebanchoA <jsag98@gmail.com>
2022-10-10 16:21:34 -0700

    Alphabets Upper

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ *main - alphabet
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+  char ch;
+  char chu;
+
+  for (ch = 'a'; ch <= 'z' ; ch++)
+    {
+      
+    putchar (ch);
+    }
+
+  for (chu = 'A'; chu <= 'Z' ; ch++)
+    {
+      putchar (chu);
+    }
+  putchar ('\n');
+  
+  return (0);
+}
