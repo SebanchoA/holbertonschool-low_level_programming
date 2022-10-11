eed3dfeb1fae816d94d0e20a06368cb00f6ad1d3
SebanchoA <jsag98@gmail.com>
2022-10-11 12:41:11 -0700

    Repito

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
   int n;
+  int r=0;
+  do{
   for( n = 'a'; n <= 'z'; n++)
     {
       _putchar(n);
       
     }
   _putchar('\n');
-
+  }while (r<10);
+    
 }
