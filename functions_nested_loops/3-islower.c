6453e1a26a29e8298e0f56cd0df3ff68723e6044
SebanchoA <jsag98@gmail.com>
2022-10-11 12:50:51 -0700

    Really Hard

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+int _islower(int c)
+{
+        
+  char c;
+  
+
+  
+  c='t';
+  
+  printf("Return value when %c is passed to islower(): %d", c, islower(c));
+  
+
+  
+  c='D';
+  
+  printf("\nReturn value when %c is passed to islower(): %d", c, islower(c));
+  
+
+  
+}
