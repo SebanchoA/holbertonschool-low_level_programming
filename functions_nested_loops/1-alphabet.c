0db979942d06ed22149d9e425fbbcc1b723afc93
SebanchoA <jsag98@gmail.com>
2022-10-11 12:24:19 -0700

    C is harder

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,10 +3,10 @@
 void print_alphabet(void)
 {
   int n;
-  for ( n = 'a'; n <= 'z'; n++)
-    
-      _putchar (n);
-      _putchar ('\n');				\
+  for( n = 'a'; n <= 'z'; n++)
+    {
+      _putchar(n);
+      _putchar('\n');				\
     }
-  
+}
 
