982a1eb4636d620abc0501f3eb80b8ec8e340e22
SebanchoA <jsag98@gmail.com>
2022-10-18 15:10:57 -0700

    Makefiles

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int i, l, temp;
+
+	l = 0;
+   	while (*s++) ++l;
+	
+	for (i = 0; i < l/2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l-1-i];
+		s[l-i-1] = temp; 
+	}
+}
+
+
+
+	
