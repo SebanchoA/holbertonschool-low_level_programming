47743f2696dbef5bb01a2cf53797cef373fcb82d
SebanchoA <jsag98@gmail.com>
2022-10-18 16:05:56 -0700

    puts2

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,26 +7,22 @@
  *
  * Return: Always 0.
  *
-*/
+ */
 
 void rev_string(char *s)
 {
 	int i, l, temp;
 
 	l = 0;
-	while (s[l] != '\0')  
+	while (s[l] != '\0')
 	{
 		l++;
 	}
-	
+
 	for (i = 0; i < l/2; i++)
 	{
 		temp = s[i];
 		s[i] = s[l-1-i];
-		s[l-i-1] = temp; 
+		s[l-i-1] = temp;
 	}
 }
-
-
-
-	
