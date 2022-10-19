e7e0c8a7169407ab63f2ef176c664214de858ec8
SebanchoA <jsag98@gmail.com>
2022-10-19 10:41:25 -0700

    New update

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -4,7 +4,7 @@
  * rev_string - a function that reverse string
  * @s: pointer
  * Return: 0.
- *
+ * 
  */
 
 void rev_string(char *s)
@@ -12,6 +12,7 @@ void rev_string(char *s)
 	int i, l, temp;
 
 	l = 0;
+
 	while (s[l] != '\0')
 	{
 		l++;
@@ -20,7 +21,7 @@ void rev_string(char *s)
 	for (i = 0; i < l/2; i++)
 	{
 		temp = s[i];
-		s[i] = s[l-1-i];
-		s[l-i-1] = temp;
+		s[i] = s[l - 1 - i];
+		s[l - i - 1] = temp;
 	}
 }
