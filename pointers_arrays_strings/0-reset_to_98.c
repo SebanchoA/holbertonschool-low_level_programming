6b250de03c9cfd6e8ecacb20a9741534feded5b0
SebanchoA <jsag98@gmail.com>
2022-10-20 15:27:58 -0700

    Cpy

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * reset_to_98 - change the value of pointer to 98
+ * @n: pointer
+ * Return: 0
  */
 
 void reset_to_98(int *n)
 {
-	*n = 98;	
+	*n = 98;
 }
