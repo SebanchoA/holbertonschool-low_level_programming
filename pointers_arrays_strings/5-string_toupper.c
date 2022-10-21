de0402d1c23f1c97ed3f4a567db755abfcdde884
SebanchoA <jsag98@gmail.com>
2022-10-21 14:49:05 -0700

    cat

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * string_toupper - uppercase the string
+ * @s: string
+ * Return: 0
  */
 
 char *string_toupper(char *s)
 {
 	int i;
-	for (i = 0; s[i]!='\0'; i++) {
-		if(s[i] >= 'a' && s[i] <= 'z') {
-			s[i] = s[i] -32;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
 		}
-}
-return (s);
+	}
+	return (s);
 }
