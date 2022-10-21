0e57bd8fe10788331519eced86cee98238211ba2
SebanchoA <jsag98@gmail.com>
2022-10-21 15:12:13 -0700

    cat

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * string_toupper - uppercase the string
+ * @s: string
+ * Return: 0
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] = s[i] - 32;
+		}
+		if (s[i] == ' ')
+		{
+			i++;
+
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+			}
+		}
+		else
+		{
+			if (s[i] >= 'A' && s[i] <= 'Z')
+				s[i] = s[i] + 32;
+		}
+	}
+	return (s);
+}
